5cd954090c1070d9180406e50efeddaa0d357705
Ragh2007 <raghavsinhavns@gmail.com>
2026-02-14 04:11:08 +0530

    Update Recruitment.jsx

--- src/Recruitment.jsx
+++ src/Recruitment.jsx
@@ -1,3 +1,4 @@
+
 import { useEffect } from "react";
 import { motion } from "framer-motion";
 import "./Recruitment.css";
@@ -331,6 +332,7 @@ const Recruitment = () => {
                     Click on any domain to view recruitment criteria and contact information
                 </motion.p>
             </div>
+        </div>
     );
 };
 
